9d4a090fc0e5a336a23a846a049d76113beed902
lisindima <lisindima1996@gmail.com>
2019-11-29 10:08:04 +0700

    Добавлен текст лицензий
    Да - да - да, я знаю, что не нужно добавлять так текст лицензий в код программы, желательно брать их из локального json, но пока их немного, они не занимают много места:)

--- altgtu/Profile/License.swift
+++ altgtu/Profile/License.swift
@@ -11,11 +11,11 @@ import SwiftUI
 struct License: View {
     var body: some View {
         Form {
-            NavigationLink(destination: LottieLicense()) {
-            Text("url-image")
+            NavigationLink(destination: URLImageLicense()) {
+                Text("URLImage")
             }
             NavigationLink(destination: LottieLicense()) {
-            Text("lottie-ios")
+                Text("Lottie")
             }
             .navigationBarTitle(Text("Лицензии"), displayMode: .inline)
         }
@@ -25,9 +25,33 @@ struct License: View {
 struct URLImageLicense: View {
     var body: some View {
         VStack {
-            Text("")
+            ScrollView {
+                Text("""
+                    MIT License
+
+                    Copyright (c) 2019 Dmytro Anokhin
+
+                    Permission is hereby granted, free of charge, to any person obtaining a copy
+                    of this software and associated documentation files (the "Software"), to deal
+                    in the Software without restriction, including without limitation the rights
+                    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+                    copies of the Software, and to permit persons to whom the Software is
+                    furnished to do so, subject to the following conditions:
+
+                    The above copyright notice and this permission notice shall be included in all
+                    copies or substantial portions of the Software.
+
+                    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+                    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+                    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+                    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+                    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+                    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+                    SOFTWARE.
+                    """).padding()
+            }
         }
-        .navigationBarTitle(Text("url-image"), displayMode: .inline)
+        .navigationBarTitle(Text("URLImage"), displayMode: .inline)
         .navigationBarItems(trailing: Button (action: {
                 UIApplication.shared.open(URL(string: "https://github.com/dmytro-anokhin/url-image")!)
             })
@@ -41,9 +65,25 @@ struct URLImageLicense: View {
 struct LottieLicense: View {
     var body: some View {
         VStack {
-            Text("")
+            ScrollView {
+                Text("""
+                    Copyright 2018 Airbnb, Inc.
+
+                    Licensed under the Apache License, Version 2.0 (the "License");
+                    you may not use this file except in compliance with the License.
+                    You may obtain a copy of the License at
+
+                    https://www.apache.org/licenses/LICENSE-2.0
+
+                    Unless required by applicable law or agreed to in writing, software
+                    distributed under the License is distributed on an "AS IS" BASIS,
+                    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+                    See the License for the specific language governing permissions and
+                    limitations under the License.
+                    """).padding()
+            }
         }
-        .navigationBarTitle(Text("lottie-ios"), displayMode: .inline)
+        .navigationBarTitle(Text("Lottie"), displayMode: .inline)
         .navigationBarItems(trailing: Button (action: {
                 UIApplication.shared.open(URL(string: "https://github.com/airbnb/lottie-ios")!)
             })
