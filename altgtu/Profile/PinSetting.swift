9ec8ca22be8413d0e4b0605aea9ad3e60cfcdf03
lisindima <lisindima1996@gmail.com>
2019-12-23 15:54:54 +0700

    bump
    Грязные наброски экрана создания пин-кода, завтра доделаю))

--- altgtu/Profile/PinSetting.swift
+++ altgtu/Profile/PinSetting.swift
@@ -7,30 +7,72 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 struct PinSetting: View {
     
     @Binding var pinCodeAccess: String
     @Binding var biometricAccess: Bool
-    @State var test1: String = ""
+    @State private var test1: String = ""
+    @State private var test2: String = ""
+    var currentType = LAContext().biometryType
+    
+    func test() {
+        pinCodeAccess = test1
+    }
     
     var body: some View {
         VStack {
             Form {
-                TextField("\(pinCodeAccess)", text: $pinCodeAccess)
-                if pinCodeAccess != "" {
-                    TextField("\(test1)", text: $test1)
+                SecureField("Пароль", text: $test1)
+                    .keyboardType(.numberPad)
+                if test1 != "" {
+                     SecureField("Повторите пароль", text: $test2)
+                        .keyboardType(.numberPad)
                 }
                 Toggle(isOn: $biometricAccess) {
-                    Text("Вход с помощью FaceID")
+                    if currentType == .faceID {
+                        Text("Вход с помощью FaceID")
+                    } else if currentType == .touchID {
+                        Text("Вход с помощью TouchID")
+                    } else {
+                        Text("Не настроенно")
+                    }
                 }
             }
-            if pinCodeAccess == test1 {
+            if test1 == test2 && test1 != "" {
                 Button("Сохранить") {
-                    
+                    self.test()
                 }
             }
         }
         .navigationBarTitle(Text("Настройка ПИН-кода"), displayMode: .inline)
+    }
+}
+
+extension LAContext {
+    enum BiometricType: String {
+        case none
+        case touchID
+        case faceID
+    }
+
+    var biometricType: BiometricType {
+        var error: NSError?
+        guard self.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
+            return .none
+        }
+        if #available(iOS 11.0, *) {
+            switch self.biometryType {
+            case .none:
+                return .none
+            case .touchID:
+                return .touchID
+            case .faceID:
+                return .faceID
+            }
+        } else {
+            return  self.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) ? .touchID : .none
+        }
     }
 }
